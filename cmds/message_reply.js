ceaf465fcf48252b22bfafe6c347fd4274329c5d
Elexuir <xanderjdavis@icloud.com>
2022-03-22 23:11:58 -0400

    beta 1.0

---
+++ cmds/message_reply.js
@@ -0,0 +1,40 @@
+module.exports = {
+  name: 'reply',
+  category: 'Bot-Related',
+  description: 'Reply\'s to a message.',
+  
+  expectedArgs: '<message>',
+  
+  minArgs: 0,
+  maxArgs: -1,
+  
+  syntaxError: {
+    'english': 'You should use `{PREFIX}`reply to use this command'
+  },
+  
+  permissions: ['ADMINISTRATOR'],
+  
+  cooldown: '60s',
+  globalCooldown: '2m',
+  
+  hidden: false,
+  ownerOnly: false,
+  
+  testOnly: false,
+  guildOnly: false,
+  
+  slash: false,
+  
+  options: [],
+  
+  error: ({
+    error,
+    command,
+    message,
+    info
+  }) => {},
+  
+  callback: ({
+    text,
+  }) => { return `${text}`}
+}
---
+++ index.js
@@ -0,0 +1,60 @@
+const discord = require('discord.js');
+const { Intents } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+const WOKCommands = require('wokcommands');
+const path = require('path');
+
+
+const client = new discord.Client({
+	// These intents are recommended for the built in help menu
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
+});
+
+client.on('ready', () => {
+	new WOKCommands(client, {
+		// The name of the local folder for your command files
+		commandsDir: path.join(__dirname, 'cmds'),
+		botOwners: ['912862728333451325', '837812033394180116'],
+		// Leave all this the same
+		disabledDefaultCommands: [
+			// 'help',
+			// 'command',
+			 'language',
+			//'prefix',
+			// 'requiredrole',
+			// 'channelonly'
+		],
+		// eslint-disable-next-line no-undef
+		mongoUri: process.env.MONGO_URI,
+		// eslint-disable-next-line no-undef
+		featuresDir: path.join(__dirname, 'features'),
+
+	})
+	  .setDefaultPrefix('-'),
+	
+		.setCategorySettings([
+			{
+				name: 'Miscellaneous',
+				emoji: '💡'
+			}, 
+			{
+				name: 'Moderation',
+				emoji: '🔨'
+			}, 
+
+			// So do it like this for making one
+			// { 
+				// name: 'What you want to name it',
+				// emoji: 'get a emoji from emojipedia',
+			//}
+		]);
+});
+
+client.login(process.env.TOKEN)
