0fcaaa1e2257e411a4b16279221aa9a35e8cd0dd
Elexuir <xanderjdavis@icloud.com>
2022-03-23 00:08:59 -0400

    update, hope this fixes it

---
+++ features/status-changer.js
@@ -0,0 +1,30 @@
+module.exports = (client) => {
+  const statusOptions = [
+        `currently in ${client.guilds.cache.size} servers`,
+        'status 2,
+        'status 3'
+    ];
+  let counter = 0;
+
+  const updateStatus = () => {
+    client.user ? .setPresence({
+      status: 'online',
+      activities: [
+        {
+          name: statusOptions[counter],
+                },
+            ],
+    });
+
+    if (++counter >= statusOptions.length) {
+      counter = 0;
+    }
+    setTimeout(updateStatus, 1000 * 60 * 5);
+  };
+  updateStatus();
+};
+
+module.exports.config = {
+  dbName: 'STATUS_CHANGER',
+  displayName: 'Status Changer',
+};
