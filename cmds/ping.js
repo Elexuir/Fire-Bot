8745f7d6f5db820a1f9a640baeb36f568afc7e6e
Elexuir <xanderjdavis@icloud.com>
2022-03-23 01:45:22 -0400

    update

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,13 +1,12 @@
 module.exports = {
   name: 'ping',
   description: 'Says Pong!',
+  category: 'Bot-Related',
   
   slash: 'both',
   testOnly: false,
   
   callback: () => {
-   return {
-     content: 'Pong!'
-   } 
+   return 'Pong!'
   }
 }
