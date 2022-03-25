bb1c54c0a3c9ff4fdb39e9ff7ce17bf90ca89046
Elexuir <xanderjdavis@icloud.com>
2022-03-25 13:21:03 -0400

    update

---
+++ cmds/verify.js
@@ -0,0 +1,31 @@
+module.exports = {
+  category: 'Moderation',
+  description: 'Verify a member to give them access to command channels.',
+  
+  slash: 'both',
+  testOnly: false,
+  
+  minArgs: 1,
+  expectedArgs: '<user>',
+  expectedArgsType: ['MEMBER'],
+  
+  callback: ({ interaction }) => {
+    const target = interaction.options.getMember('user')
+    
+    if(interaction.guild.id != '956389530364162101') {
+      return interaction.reply({
+        custom: true,
+        content: 'You cannot use this command in this server.',
+        ephemeral: true
+      })
+    }
+    
+    target.roles.add('956962605497466910')
+    
+    return interaction.reply({
+      custom: true,
+      content: `<@${target.id}> has been Verified!`,
+      ephemeral: true
+    })
+  }
+}
